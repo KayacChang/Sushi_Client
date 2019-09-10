bbffbfa9519b6f52d332e54f8a01166dc0e7c6cb
KayacChang <egg734631@gmail.com>
2019-09-10 11:00:04 +0800

    Add

--- project/src/game/interface/data/sprites.js
+++ project/src/game/interface/data/sprites.js
@@ -1,17 +1,13 @@
 import INTERFACE_URL from '../assets/sprite_sheets/interface.fui';
 import INTERFACE_ATLAS0_URL from '../assets/sprite_sheets/interface@atlas0.png';
-import INTERFACE_ATLAS0_1_URL from '../assets/sprite_sheets/interface@atlas0_1.png';
-import INTERFACE_ATLAS0_2_URL from '../assets/sprite_sheets/interface@atlas0_2.png';
-import INTERFACE_ATLAS0_3_URL from '../assets/sprite_sheets/interface@atlas0_3.png';
-import INTERFACE_ATLAS0_4_URL from '../assets/sprite_sheets/interface@atlas0_4.png';
-import INTERFACE_ATLAS0_5_URL from '../assets/sprite_sheets/interface@atlas0_5.png';
+import INTERFACE_ATLAS1_URL from '../assets/sprite_sheets/interface@atlas1.png';
+import INTERFACE_ATLAS1_1_URL from '../assets/sprite_sheets/interface@atlas1_1.png';
+import INTERFACE_ATLAS1_2_URL from '../assets/sprite_sheets/interface@atlas1_2.png';
 
 export const sprites = [
     {name: 'assets.fui', url: INTERFACE_URL, xhrType: 'arraybuffer'},
     {name: 'assets@atlas0.png', url: INTERFACE_ATLAS0_URL},
-    {name: 'assets@atlas0_1.png', url: INTERFACE_ATLAS0_1_URL},
-    {name: 'assets@atlas0_2.png', url: INTERFACE_ATLAS0_2_URL},
-    {name: 'assets@atlas0_3.png', url: INTERFACE_ATLAS0_3_URL},
-    {name: 'assets@atlas0_4.png', url: INTERFACE_ATLAS0_4_URL},
-    {name: 'assets@atlas0_5.png', url: INTERFACE_ATLAS0_5_URL},
+    {name: 'assets@atlas1.png', url: INTERFACE_ATLAS1_URL},
+    {name: 'assets@atlas1_1.png', url: INTERFACE_ATLAS1_1_URL},
+    {name: 'assets@atlas1_2.png', url: INTERFACE_ATLAS1_2_URL},
 ];
